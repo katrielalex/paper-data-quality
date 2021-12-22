538cf10b2da19ebe61ae4c02db24be8c0f7a047a
Katriel Cohn-Gordon <me@katriel.co.uk>
2021-12-22 21:49:50 +0000

    add mui and fancy svg thing

--- pages/dq/[...doia].js
+++ pages/dq/[...doia].js
@@ -4,6 +4,10 @@ import Head from "next/head";
 import Footer from "@components/Footer";
 import Header from "@components/Header";
 
+import Typography from "@mui/material/Typography";
+import SvgIcon from "@mui/material/SvgIcon";
+import Link from "next/link";
+
 export async function getServerSideProps(context) {
   let uri = new URL("https://api.openaccessbutton.org/metadata");
   uri.searchParams.append("id", "10.1007/s00145-020-09360-1");
@@ -15,6 +19,14 @@ export async function getServerSideProps(context) {
       data: { doi: context.params.doia?.join("/"), metadata: metadata },
     },
   };
+}
+
+function LightBulbIcon(props) {
+  return (
+    <SvgIcon {...props}>
+      <path d="M9 21c0 .55.45 1 1 1h4c.55 0 1-.45 1-1v-1H9v1zm3-19C8.14 2 5 5.14 5 9c0 2.38 1.19 4.47 3 5.74V17c0 .55.45 1 1 1h6c.55 0 1-.45 1-1v-2.26c1.81-1.27 3-3.36 3-5.74 0-3.86-3.14-7-7-7zm2.85 11.1l-.85.6V16h-4v-2.3l-.85-.6C7.8 12.16 7 10.63 7 9c0-2.76 2.24-5 5-5s5 2.24 5 5c0 1.63-.8 3.16-2.15 4.1z" />
+    </SvgIcon>
+  );
 }
 
 const Post = ({ data }) => {
@@ -40,6 +52,10 @@ const Post = ({ data }) => {
               </a>{" "}
               ({data.metadata.journal})
             </p>
+            <Typography sx={{ mt: 6, mb: 3 }} color="text.secondary">
+              <LightBulbIcon sx={{ mr: 1, verticalAlign: "middle" }} />
+              Paper not found in lookup
+            </Typography>{" "}
           </>
         )}
       </main>
